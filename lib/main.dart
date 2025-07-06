6cfba644be603b7df857776025e25ebcc9b31f8b
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-07-06 11:15:09 +0530

    go router setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:study_planner_u6_9/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:study_planner_u6_9/router/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,8 +15,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: "Study Planner",
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      routerConfig: RouterClass.router,
     );
   }
 }
