b3fd24f4de057d7878a195842eae0aa309c817f7
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-07-06 12:50:01 +0530

    bottom navigation bar done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:study_planner_u6_9/constants/colors.dart';
+import 'package:study_planner_u6_9/pages/main_screens/assignments_page.dart';
+import 'package:study_planner_u6_9/pages/main_screens/courses_page.dart';
+import 'package:study_planner_u6_9/pages/main_screens/main_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +12,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> pages = [
+    MainPage(),
+    CoursesPage(),
+    AssignmentsPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Study Planner"),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (value) {
+          setState(() {
+            _selectedIndex = value;
+          });
+        },
+        selectedItemColor: primaryColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: "Course",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment_rounded),
+            label: "Assignment",
+          ),
+        ],
       ),
+      body: pages[_selectedIndex],
     );
   }
 }
---
+++ lib/pages/main_screens/courses_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CoursesPage extends StatelessWidget {
+  const CoursesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("Courses Page"),
+    ));
+  }
+}
